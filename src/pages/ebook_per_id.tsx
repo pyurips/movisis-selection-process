5e974fe4c51e861a8b7430d7d201ae8282df5c25
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-22 17:14:16 -0300

    hotfix: ebook per id

--- src/pages/ebook_per_id.tsx
+++ src/pages/ebook_per_id.tsx
@@ -5,9 +5,18 @@ export default function EbookPerId() {
 
   return (
     <section className="flex flex-col gap-10">
-      <section className="flex flex-col gap-3"></section>
+      <section className="flex flex-row">
+        <img
+          className="w-[350px] h-[500px] object-contain bg-[red] rounded-xl"
+          src=""
+          alt=""
+        />
+        <div className="flex flex-1 bg-[black]"></div>
+      </section>
 
-      <section className="flex flex-col gap-3"></section>
+      <section className="flex flex-col gap-3">
+        
+      </section>
     </section>
   );
 }
