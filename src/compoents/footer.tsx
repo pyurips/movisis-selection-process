c87f8cf2f40fac34c60b838f33bd20b7613e3342
Pablo Yuri <pabloyuridev@gmail.com>
2024-01-22 02:03:49 -0300

    feat: added footer configs

--- src/compoents/footer.tsx
+++ src/compoents/footer.tsx
@@ -1,3 +1,17 @@
+import { Link } from "@nextui-org/react";
+
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="flex flex-row justify-between py-10">
+      <section className="flex flex-col gap-3">
+        <p className="text-sm text-stone-500">
+          © 2023 Readify. Todos direitos reservados.
+        </p>
+        <Link size="sm" href="#">Política de Privacidade</Link>
+        <Link size="sm" href="#">Termos de Uso</Link>
+      </section>
+
+      <section></section>
+    </footer>
+  );
 }
